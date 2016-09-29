52aa8e2bfd41deed20a57ec3ea030f7915449137
Ally Dale <vipally@163.com>
2016-09-29 18:28:34 +0800

    update example

--- example/ping/main.go
+++ example/ping/main.go
@@ -11,26 +11,32 @@ func main() {
 		target_name string
 		v4          = false
 		ttl         = 0
-		n           = 0
+		c           = 0
 	)
-	cmdline.Summary("<thisCmd> is an example of command line usage to test gitbub.com/vipally.cmdline package.")
+	cmdline.Summary("<thiscmd> is an example usage of gitbub.com/vipally.cmdline package.")
 	cmdline.Details("no details")
 	cmdline.CopyRight("no copyright in example cmd")
 
+	//noname flag and require ones
 	cmdline.StringVar(&target_name, "", "target_name", "", true, "target host ip or name")
+
 	cmdline.BoolVar(&v4, "4", "v4", v4, false, "ipv4")
+
+	//synonym with the same variables
 	cmdline.IntVar(&ttl, "t", "ttl", ttl, false, "ttl")
 	cmdline.IntVar(&ttl, "ttl", "synonym of -t", ttl, true, "this is synonym of -t, no mater thats this")
 
-	cmdline.IntVar(&n, "c", "count", n, false, "count")
+	//define a synonym with method AnotherName
+	cmdline.IntVar(&c, "c", "count", c, false, "count")
 	cmdline.AnotherName("count", "c")
+
 	cmdline.Parse()
 
 	fmt.Println(cmdline.GetUsage())
 
 	//Usage of ([ping] Build [Sep 29 2016 18:19:06]):
 	//  Summary:
-	//    <thisCmd> is an example of command line usage to test gitbub.com/vipally.cmdline package.
+	//    ping is an example of command line usage to test gitbub.com/vipally.cmdline package.
 
 	//  Usage:
 	//    ping [-4=<v4>] [-c|count=<count>] [-t|ttl=<ttl>] <target_name>
